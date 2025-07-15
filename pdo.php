a7c523bbae5e2ff0a32f18a43195fae1a9fb383b
morainstein <nichroberto@gmail.com>
2025-07-14 21:26:32 -0300

    aula dia 14/jul

--- pdo.php
+++ pdo.php
@@ -1,11 +1,13 @@
 <?php
 
-  // require_once 'config.php';
+  require_once 'config.php';
 
-  // try {
-  //   $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-  // } catch (PDOException $e) {
-  //   echo "Erro com o banco de dados: ".$e->getMessage();
-  // }
+function conn()
+{   
+  try {
+    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+  } catch (PDOException $e) {
+    echo "Erro com o banco de dados: ".$e->getMessage();
+  }
+}
   
-  // var_dump($pdo);
--- public/index.php
+++ public/index.php
@@ -1,16 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-  define("DB_NAME", "php");
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "123");
-
-  try {
-    $pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-  } catch (PDOException $e) {
-    echo "Erro com o banco de dados: ".$e->getMessage();
-  }
+$pdo = conn();
 
 $rota = $_SERVER['REQUEST_URI'] ?? "/";
 $metodo = $_SERVER['REQUEST_METHOD'];
